560ac804729f995c10157ab1745fbccf56f92d7d
lmalms <87232413+lmalms@users.noreply.github.com>
2022-01-27 23:00:29 +0000

    task 01

---
+++ tests/test_operators.py
@@ -0,0 +1,20 @@
+from hypothesis import given
+from hypothesis.strategies import lists
+from operators import (
+    mul,
+    id,
+    add,
+    neg,
+    lt,
+    eq,
+    max,
+    is_close,
+    sigmoid,
+    relu,
+    log,
+    exp,
+    log_back,
+    inv,
+    inv_back,
+    relu_back
+)
