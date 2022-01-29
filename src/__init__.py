6bbe62d051ebd2dca4fbfc788829dd6546f57b9a
lmalms <87232413+lmalms@users.noreply.github.com>
2022-01-29 11:23:25 +0000

    tests for task01

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="minitorch",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "hypothesis==4.38.0"
+    ]
+)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,6 +1,8 @@
 from hypothesis import given
 from hypothesis.strategies import lists
-from operators import (
+import pytest
+
+from minitorch.operators import (
     mul,
     id,
     add,
@@ -16,5 +18,36 @@ from operators import (
     log_back,
     inv,
     inv_back,
-    relu_back
+    relu_back,
 )
+from tests.strategies import small_floats, assert_close
+
+
+# Hypothesis tests for basic operators
+@given(small_floats, small_floats)
+def test_same_as_python(x: float, y: float) -> None:
+    """Check that basic operators are the same as python implementation."""
+    assert_close(mul(x, y), x * y)
+    assert_close(add(x, y), x + y)
+    assert_close(neg(x), -x)
+    assert_close(max(x, y), x if x > y else y)
+    if x != 0.:
+        assert_close(inv(x), 1./x)
+
+
+@given(small_floats)
+def test_relu(x: float) -> None:
+    if x > 0.:
+        assert relu(x) == x
+    else:
+        assert relu(x) == 0.
+
+
+@given(small_floats, small_floats)
+def test_relu_back(x: float, y: float) -> None:
+    if x > 0.0:
+        assert relu_back(x, y) == y
+    else:
+        assert relu_back(x, y) == 0.
+
+
