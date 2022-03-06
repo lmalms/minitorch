4a311a488bc78846df065bd416a19363c63b6b7b
lmalms <87232413+lmalms@users.noreply.github.com>
2022-03-06 00:16:07 +0000

    parameter class added

---
+++ src/minitorch/parameter.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class Parameter:
+
+    def __init__(self, value=None, name: Optional[str] = None):  #TODO: add type info for value
+        self.value = value
+        self.name = name
+        self.update(value=value)
+
+    def update(self, value):
+        """
+        Updates the parameters value.
+        """
+        self.value = value
+        if hasattr(value, "requires_grad_"):
+            self.value.requires_grad_(True)
+            if self.name:
+                self.value.name = self.name
+
+    def __repr__(self):
+        return repr(self.value)
+
+    def __str__(self):
+        return str(self.value)
