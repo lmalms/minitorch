f8dff7013c2f2baacf23343370e00cef385f39c7
lmalms <87232413+lmalms@users.noreply.github.com>
2022-05-09 22:40:03 +0100

    small refactor

--- src/minitorch/autodiff/utils.py
+++ src/minitorch/autodiff/utils.py
@@ -2,12 +2,15 @@ from typing import Union, Tuple, Any
 
 
 def wrap_tuple(x: Any) -> Tuple[Any]:
+    # TODO: validate types here.
     if isinstance(x, tuple):
         return x
     return (x, )
 
 
-def unwrap_tuple(x: Union[Tuple[Any], Any]) -> Any:
-    if len(x) == 1:
+def unwrap_tuple(x: Union[Tuple[float], float]) -> float:
+    # TODO: have some type validation here.
+    # TODO: check are these really floats? or Variables?
+    if (isinstance(x, tuple)) and (len(x) == 1):
         return x[0]
     return x
--- src/minitorch/autodiff/variable.py
+++ src/minitorch/autodiff/variable.py
@@ -10,10 +10,11 @@ VARIABLE_COUNT = 1
 
 class Context:
     """
-    The context class is used by Functions to store information during the forward pass.
+    The context class is used by Functions to store information during the forward pass, which in turn is needed
+    for computations during the backward pass.
 
     Attributes:
-        no_grad - bool
+        requires_grad_ - bool
             Whether to save gradient information or not.
         saved_values - Tuple[]
             A tuple of values saved for backward pass.
@@ -22,27 +23,26 @@ class Context:
 
     """
 
-    def __init__(self, no_grad: bool = False):
-        self.no_grad = no_grad
+    def __init__(self, requires_grad_: bool = False):
+        self.requires_grad_ = requires_grad_
         self._saved_values = None
 
     @property
     def saved_values(self):
-        assert not self.no_grad, "Doesn't require grad."
-        assert self._saved_values is not None, "Did you forget to save values?"
+        assert self.requires_grad_, "No gradients required - no values saved."
+        assert self._saved_values is not None, "No values saved - did you forget to save values?"
         return unwrap_tuple(self._saved_values)
 
     @property
     def saved_tensors(self):
         return self.saved_values
 
-    def save_for_backward(self, *values):
+    def save_for_backward(self, *values) -> None:
         """
         Stores the given values if they need to be used during back-propagation
         """
-        if self.no_grad:
-            return None
-        self._saved_values = values
+        if self.requires_grad_:
+            self._saved_values = values
 
 
 class History:
@@ -127,7 +127,7 @@ class BaseFunction:
                 raw_values.append(v)
 
         # Create context
-        ctx = Context(no_grad=not need_grad)
+        ctx = Context(requires_grad_=not need_grad)
 
         # Call forward with variables
         c = cls.forward(ctx, *raw_values)
