d2dd9a4878fa3bf358a784ff10d0c4f1d2b3bf0a
lmalms <87232413+lmalms@users.noreply.github.com>
2022-07-28 12:01:19 +0100

    implemented breadth first search

--- src/minitorch/autodiff/topological_sort.py
+++ src/minitorch/autodiff/topological_sort.py
@@ -28,11 +28,39 @@ def topological_sort(variable: Union[Variable, float]) -> List[Variable]:
             for v in variable.history.inputs:
                 dfs_visit(v)
 
+    def visit_variable_and_add(variable):
+        if not isinstance(variable, Variable):
+            return
+
+        if variable.is_constant():
+            return
+
+        if hasattr(variable, "seen") and variable.seen:
+            return
+
+        # Append variable to list
+        diff_chain.append(variable)
+
+        # Mark variable as visited
+        setattr(variable, "seen", True)
+
+    def bfs_visit(variable: Variable) -> None:
+
+        # Append all of its children to list
+        if variable.history.inputs is not None:
+            for v in variable.history.inputs:
+                visit_variable_and_add(v)
+
+            # Run bfs_visit on children
+            for v in variable.history.inputs:
+                bfs_visit(v)
+
     def remove_seen(variables: List[Variable]) -> None:
         for variable in variables:
             if hasattr(variable, "seen"):
                 delattr(variable, "seen")
 
-    dfs_visit(variable)
+    visit_variable_and_add(variable)
+    bfs_visit(variable)
     remove_seen(diff_chain)
     return diff_chain
