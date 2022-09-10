fb578f2a989e7cf9000f72e03e1c1327f878eb6d
lmalms <87232413+lmalms@users.noreply.github.com>
2022-09-10 13:55:05 +0100

    tests for utils

--- src/minitorch/utils.py
+++ src/minitorch/utils.py
@@ -5,3 +5,11 @@ from minitorch.autodiff import Scalar
 
 def check_for_equal_dim(y_true: List[Scalar], y_hat: List[Scalar]):
     assert len(y_true) == len(y_hat), "y_true and y_hat need to have the same length."
+
+
+def check_for_binary_values(y: List[Scalar]):
+
+    def _is_binary(y: List[Scalar]) -> bool:
+        return all((s.data == 0) or (s.data == 1) for s in y)
+
+    assert _is_binary(y), "y can only contain scalars of value 1. or 0."
---
+++ tests/test_utils.py
@@ -0,0 +1,66 @@
+from typing import List
+
+import pytest
+
+from minitorch.autodiff import Scalar
+from minitorch.utils import check_for_binary_values, check_for_equal_dim
+
+
+@pytest.mark.parametrize(
+    "y",
+    [
+        [Scalar(1), Scalar(1), Scalar(1)],
+        [Scalar(0), Scalar(0), Scalar(0)],
+        [Scalar(1), Scalar(0), Scalar(0)],
+    ],
+)
+def test_binary_values_check(y: List[Scalar]) -> None:
+    check_for_binary_values(y)
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    "y",
+    [
+        [Scalar(1.1), Scalar(1), Scalar(1)],
+        [Scalar(0), Scalar(0.5), Scalar(0)],
+        [Scalar(1), Scalar(0.3), Scalar(0)],
+    ],
+)
+def test_binary_values_check_fail(y: List[Scalar]) -> None:
+    check_for_binary_values(y)
+
+
+@pytest.mark.parametrize(
+    ["x", "y"],
+    [
+        (
+            [Scalar(1), Scalar(1)],
+            [Scalar(0), Scalar(0)],
+        ),
+        (
+            [Scalar(1)],
+            [Scalar(0)],
+        ),
+    ],
+)
+def test_equal_dims_check(x: List[Scalar], y: List[Scalar]) -> None:
+    check_for_equal_dim(x, y)
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    ["x", "y"],
+    [
+        (
+            [Scalar(1)],
+            [Scalar(0), Scalar(0)],
+        ),
+        (
+            [Scalar(1)],
+            [Scalar(0), Scalar(1)],
+        ),
+    ],
+)
+def test_equal_dims_check_fail(x: List[Scalar], y: List[Scalar]) -> None:
+    check_for_equal_dim(x, y)
