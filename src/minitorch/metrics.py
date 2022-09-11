abba673ae90c4a7f4f36a792f05338d014b3e84a
lmalms <87232413+lmalms@users.noreply.github.com>
2022-09-11 18:18:48 +0100

    roc curve

--- src/minitorch/metrics.py
+++ src/minitorch/metrics.py
@@ -1,7 +1,8 @@
-from typing import List, Tuple
+from typing import List
 
 from minitorch.autodiff import Scalar
 from minitorch.functional import summation
+from minitorch.types import RocCurveResult
 from minitorch.utils import check_for_binary_values, check_for_equal_dim
 
 
@@ -51,26 +52,41 @@ def specificity(y_true: List[Scalar], y_hat: List[Scalar]) -> Scalar:
 
 
 def roc_curve(
-    y_true: List[Scalar], y_hat: List[Scalar]
-) -> Tuple[List[Scalar], List[Scalar], List[Scalar]]:
-    # y_hat here should be the predicted probabilities of a particular
-    # sample belonging to the positive class
+    y_true: List[Scalar], y_hat: List[Scalar], bucket_size: float = 1e-03
+) -> RocCurveResult:
+    """
+    Computes ROC curve, that is a plot of true positive and false positive rates as a function of classification
+    threshold.
+
+    Args:
+         y_true - List[Scalar]
+            The true probabilities for the positive class.
+        y_hat - List[Scalar]
+            The predicted probabilities for the positive class.
+        bucket_size - float, default = 1e-03
+            The threshold interval at which to compute the true positive and false positive rates.
+            Note: Has to be between 1e-03 and 1.
+    """
     check_for_equal_dim(y_true, y_hat)
 
-    # Infer the thresholds at which to compute tpr and fpr
-    delta = 1e-06
-    # TODO: Can I bucket the thresholds somehow?
-    # 1. sort in ascending order
-    y_true, y_hat = zip(
-        *[
-            (y_t, y_h)
-            for (y_t, y_h) in sorted(zip(y_true, y_hat), key=lambda pair: pair[1])
-        ]
-    )
-    thresholds = [y - delta for y in y_hat]
+    def bucket_thresholds(y_hat: List[Scalar], bucket_size: float = 1e-03):
+        bucket_size = min(max(bucket_size, 0.001), 1.0)
+        thresholds = [0.0]
+        for y_h in y_hat:
+            if max(thresholds) < (y_h - bucket_size):
+                thresholds.append(y_h)
+
+        return thresholds
+
+    sorted_pairs = sorted(zip(y_true, y_hat), key=lambda pair: pair[1])
+    y_true, y_hat = zip(*[(y_t, y_h) for (y_t, y_h) in sorted_pairs])
+    thresholds = bucket_thresholds(y_hat, bucket_size)
+
     tpr, fpr = [], []
     for threshold in thresholds:
-        y_hat_at_threshold = [1.0 if (proba >= threshold) else 0.0 for proba in y_hat]
+        y_hat_at_threshold = [
+            Scalar(1.0) if (proba >= threshold) else Scalar(0.0) for proba in y_hat
+        ]
         tpr_at_threshold = true_positive_rate(y_true, y_hat_at_threshold)
         fpr_at_threshold = false_positive_rate(y_true, y_hat_at_threshold)
         tpr.append(tpr_at_threshold)
