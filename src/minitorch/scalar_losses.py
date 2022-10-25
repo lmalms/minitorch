1f6312f3b0c0ba92de7771332f215dd1ca8d4b05
lmalms <87232413+lmalms@users.noreply.github.com>
2022-10-25 22:58:35 +0100

    refactor

--- src/minitorch/scalar_losses.py
+++ src/minitorch/scalar_losses.py
@@ -2,11 +2,14 @@ from typing import List
 
 from minitorch.autodiff import Scalar
 from minitorch.functional import summation
-from minitorch.utils import check_for_equal_dim
+
+
+def _check_for_equal_dim(y_true: List[Scalar], y_hat: List[Scalar]):
+    assert len(y_true) == len(y_hat), "y_true and y_hat need to have the same length."
 
 
 def mean_squared_error(y_true: List[Scalar], y_hat: List[Scalar]) -> Scalar:
-    check_for_equal_dim(y_true, y_hat)
+    _check_for_equal_dim(y_true, y_hat)
     residuals = [(y_t - y_h).square() for (y_t, y_h) in zip(y_true, y_hat)]
     mse = summation(residuals) / len(y_true)
     return mse
@@ -22,7 +25,7 @@ def binary_cross_entropy(y_true: List[Scalar], y_hat: List[Scalar]) -> Scalar:
         y_hat - List[Scalar]
             The predicted probabilities for the positive class.
     """
-    check_for_equal_dim(y_true, y_hat)
+    _check_for_equal_dim(y_true, y_hat)
     log_likelihoods = [
         y_h.log() if y_t == 1.0 else (-y_h + 1).log()
         for (y_t, y_h) in zip(y_true, y_hat)
---
+++ src/minitorch/tensor_losses.py
@@ -0,0 +1,9 @@
+from minitorch.autodiff import Tensor
+
+
+def _check_dims():
+    pass
+
+
+def mean_squared_error(y_true: Tensor, y_hat: Tensor) -> Tensor:
+    pass
