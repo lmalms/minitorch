1a5e431511c07f7b6cd0a26b84a23c93f27ec6e8
lmalms <87232413+lmalms@users.noreply.github.com>
2022-10-25 21:39:19 +0100

    tests for tensor network init

--- tests/test_network.py
+++ tests/test_network.py
@@ -8,11 +8,12 @@ from hypothesis import given
 
 from minitorch.autodiff import Scalar
 from minitorch.module import LinearScalarLayer, ScalarNetwork
+from minitorch.module.network import TensorNetwork
 from minitorch.operators import relu
 
 from .strategies import medium_ints
 
-SKIP_NETWORK_FORWARD_TESTS = False
+SKIP_NETWORK_FORWARD_TESTS = True
 SKIP_REASON = "Tests are slow."
 
 
@@ -53,14 +54,6 @@ def test_scalar_network_forward(input_dim: int, hidden_dim: int, output_dim: int
         y_hat = network.forward(input_)
         return np.array([[scalar.data for scalar in row] for row in y_hat])
 
-    def np_forward_single(
-        weights: np.ndarray, biases: np.ndarray, input_: List[List[float]]
-    ) -> np.ndarray:
-        # Note: check that last layer in network.py also applies relu activation!
-        out_ = np.dot(np.array(input_), weights) + biases
-        out_ = [[relu(x) for x in row] for row in out_]
-        return np.array(out_)
-
     def np_forward(input_: List[List[float]]) -> np.ndarray:
         # Input to hidden
         ih_state = np.dot(np.array(input_), ih_weights) + ih_bias
@@ -88,3 +81,27 @@ def test_scalar_network_forward(input_dim: int, hidden_dim: int, output_dim: int
 
     X_scalars = [[Scalar(value) for value in row] for row in X]
     assert np.allclose(minitorch_forward(X_scalars), np_forward(X))
+
+
+@given(medium_ints, medium_ints, medium_ints)
+def test_tensor_network_init(input_dim: int, hidden_dim: int, output_dim: int):
+    network = TensorNetwork(input_dim, hidden_dim, output_dim)
+
+    # Check dimensions of weight and bias matrices
+    # Input layer
+    in_weights = network._input_layer._weights
+    in_bias = network._input_layer._bias
+    assert in_weights.value.shape == (input_dim, hidden_dim)
+    assert in_bias.value.shape == (hidden_dim,)
+
+    # Hidden layer
+    hidden_weights = network._hidden_layer._weights
+    hidden_bias = network._hidden_layer._bias
+    assert hidden_weights.value.shape == (hidden_dim, hidden_dim)
+    assert hidden_bias.value.shape == (hidden_dim,)
+
+    # Output layer
+    out_weights = network._output_layer._weights
+    out_bias = network._output_layer._bias
+    assert out_weights.value.shape == (hidden_dim, output_dim)
+    assert out_bias.value.shape == (output_dim,)
