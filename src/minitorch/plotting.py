2e164e7c2710a0725487d084f37b4c77403e50ae
lmalms <87232413+lmalms@users.noreply.github.com>
2022-11-15 00:00:16 +0000

    update diagonal plotting

--- src/minitorch/plotting.py
+++ src/minitorch/plotting.py
@@ -44,46 +44,46 @@ def plot_scalar_predictions(
                 [x1_lower for _ in range(x2_res + 1)],
                 alpha=(y_mean - 0.5) if y_mean >= 0.5 else (0.5 - y_mean),
                 color="tab:blue" if y_mean >= 0.5 else "tab:red",
-                lw=0.01,
+                lw=0.001,
             )
 
     elif dataset.type == "diagonal":
 
-        bias_positions = list(np.linspace(0, 2, 101))
-        x_range = list(np.linspace(0, 1, 101))
+        x1_res, bias_res = 100, 100
+        x_positions = list(np.linspace(0, 1, x1_res + 1))
+        bias_positions = list(np.linspace(0, 2, bias_res + 1))
 
         for bias_lower, bias_upper in zip(bias_positions, bias_positions[1:]):
 
             # Create corresponding x2 values for lower
-            X_lower = [list((x1, bias_lower - x1)) for x1 in x_range]
+            X_lower = [list((x1, bias_lower - x1)) for x1 in x_positions]
 
             # Clamp values to between 0.0 and 1.0
             X_lower = [[x1, max(min(x2, 1.0), 0.0)] for (x1, x2) in X_lower]
             y_lower = model.forward(X_lower)
-            y_mean_lower = sigmoid(
-                sum(scalar[0].data for scalar in y_lower) / len(y_lower)
-            )
+            y_lower = list(itertools.chain.from_iterable(y_lower))
+            y_mean_lower = sigmoid(sum(s.data for s in y_lower) / len(y_lower))
 
             # Create corresponding x2 values for upper
-            X_upper = [list((x1, bias_upper - x1)) for x1 in x_range]
+            X_upper = [list((x1, bias_upper - x1)) for x1 in x_positions]
 
             # Clamp upper and lower bounds
             X_upper = [[x1, max(min(x2, 1.0), 0.0)] for (x1, x2) in X_upper]
             y_upper = model.forward(X_upper)
-            y_mean_upper = sigmoid(
-                sum(scalar[0].data for scalar in y_upper) / len(y_upper)
-            )
+            y_upper = list(itertools.chain.from_iterable(y_upper))
+            y_mean_upper = sigmoid(sum(s.data for s in y_upper) / len(y_upper))
+
+            # Average
+            y_mean = (y_mean_upper + y_mean_lower) / 2
 
             # Plot and fill
             ax.fill_between(
-                x_range,
+                x_positions,
                 [x2 for (_, x2) in X_lower],
                 [x2 for (_, x2) in X_upper],
-                alpha=(y_mean_lower - 0.5)
-                if y_mean_lower >= 0.5
-                else (0.5 - y_mean_lower),
-                color="tab:blue" if y_mean_lower >= 0.5 else "tab:red",
-                lw=0.01,
+                alpha=(y_mean - 0.5) if y_mean >= 0.5 else (0.5 - y_mean),
+                color="tab:blue" if y_mean >= 0.5 else "tab:red",
+                lw=0.001,
             )
 
     elif dataset.type == "split":
