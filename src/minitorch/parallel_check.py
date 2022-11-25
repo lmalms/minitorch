91fe6bab69e7fec279c9c28f17b162f57c34cfda
Lukas Malms <lukas.malms@gmail.com>
2022-11-25 11:04:03 +0000

    check for tensor zip

--- src/minitorch/parallel_check.py
+++ src/minitorch/parallel_check.py
@@ -10,3 +10,11 @@ tmap = fast_ops.tensor_map(njit()(ops.identity))
 out_, in_ = tf.zeros((10,)), tf.zeros((10,))
 tmap(*out_.tuple(), *in_.tuple())
 print(tmap.parallel_diagnostics(level=3))
+
+
+# ZIP
+print("ZIP")
+out, a, b = tf.zeros((10,)), tf.zeros((10,)), tf.zeros((10,))
+tzip = fast_ops.tensor_zip(njit()(ops.eq))
+tzip(*out.tuple(), *a.tuple(), *b.tuple())
+print(tzip.parallel_diagnostics(level=3))
