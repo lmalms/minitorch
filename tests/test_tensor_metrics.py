f542cd75038532232a6224c61345ea9593aa509b
Lukas Malms <lukas.malms@gmail.com>
2022-12-06 22:18:11 +0000

    test for tensor metrics with different backends

--- tests/test_tensor_metrics.py
+++ tests/test_tensor_metrics.py
@@ -3,95 +3,139 @@ from typing import List
 import pytest
 
 import minitorch.autodiff.tensor_functions as tf
-from minitorch.autodiff import Tensor
+from minitorch.autodiff import FastOps, SimpleOps, Tensor, TensorBackend
 from minitorch.tensor_metrics import accuracy, false_positive_rate, true_positive_rate
 
+# Define tensor backends
+BACKENDS = {"simple": TensorBackend(SimpleOps), "fast": TensorBackend(FastOps)}
+
 
 @pytest.mark.parametrize(
-    ["y_true", "y_hat", "true_acc"],
+    ["y_true", "y_hat", "expected_acc"],
     [
         (
-            tf.ones((4,)),
-            tf.ones((4,)),
-            tf.ones((1,)),
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0],
+        ),
+        ([1.0, 1.0, 1.0, 1.0], [0.0, 0.0, 0.0, 0.0], [0.0]),
+        (
+            [1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.5],
         ),
         (
-            tf.ones((4,)),
-            tf.zeros((4,)),
-            tf.zeros((1,)),
-        ),
-        (
-            tf.ones((4,)),
-            tf.tensor([0.0, 0.0, 1.0, 1.0]),
-            tf.tensor([0.5]),
-        ),
-        (
-            tf.ones((4,)),
-            tf.tensor([1.0, 0.0, 1.0, 1.0]),
-            tf.tensor([0.75]),
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0, 0.0, 1.0, 1.0],
+            [0.75],
         ),
     ],
 )
-def test_accuracy(y_true: Tensor, y_hat: Tensor, true_acc: Tensor):
+@pytest.mark.parametrize(
+    "backend",
+    (
+        pytest.param("simple"),
+        pytest.param("fast"),
+    ),
+)
+def test_accuracy(
+    y_true: List[float],
+    y_hat: List[float],
+    expected_acc: List[float],
+    backend: str,
+):
+    y_true = tf.tensor(y_true, backend=BACKENDS[backend])
+    y_hat = tf.tensor(y_hat, backend=BACKENDS[backend])
+    expected_acc = tf.tensor(expected_acc, backend=BACKENDS[backend])
     predicted_acc = accuracy(y_true, y_hat)
-    assert bool(predicted_acc.is_close(true_acc).item())
+    assert bool(predicted_acc.is_close(expected_acc).item())
 
 
 @pytest.mark.parametrize(
-    ["y_true", "y_hat", "true_tpr"],
+    ["y_true", "y_hat", "expected_tpr"],
     [
         (
-            tf.ones((4,)),
-            tf.ones((4,)),
-            tf.ones((1,)),
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0],
         ),
         (
-            tf.ones((4,)),
-            tf.zeros((4,)),
-            tf.zeros((1,)),
+            [1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0],
         ),
         (
-            tf.ones((4,)),
-            tf.tensor([0.0, 0.0, 1.0, 1.0]),
-            tf.tensor([0.5]),
+            [1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.5],
         ),
         (
-            tf.tensor([0.0, 0.0, 1.0, 1.0]),
-            tf.tensor([0.0, 1.0, 1.0, 1.0]),
-            tf.tensor([1.0]),
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 1.0, 1.0, 1.0],
+            [1.0],
         ),
     ],
 )
-def test_true_positive_rate(y_true: Tensor, y_hat: Tensor, true_tpr: Tensor):
+@pytest.mark.parametrize(
+    "backend",
+    (
+        pytest.param("simple"),
+        pytest.param("fast"),
+    ),
+)
+def test_true_positive_rate(
+    y_true: List[float],
+    y_hat: List[float],
+    expected_tpr: List[float],
+    backend: str,
+):
+    y_true = tf.tensor(y_true, backend=BACKENDS[backend])
+    y_hat = tf.tensor(y_hat, backend=BACKENDS[backend])
+    expected_tpr = tf.tensor(expected_tpr, backend=BACKENDS[backend])
     predicted_tpr = true_positive_rate(y_true, y_hat)
-    assert bool(predicted_tpr.is_close(true_tpr).item())
+    assert bool(predicted_tpr.is_close(expected_tpr).item())
 
 
 @pytest.mark.parametrize(
-    ["y_true", "y_hat", "true_fpr"],
+    ["y_true", "y_hat", "expected_fpr"],
     [
         (
-            tf.ones((4,)),
-            tf.ones((4,)),
-            tf.zeros((1,)),
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0],
+            [0.0],
         ),
         (
-            tf.ones((4,)),
-            tf.zeros((4,)),
-            tf.zeros((1,)),
+            [1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0],
         ),
         (
-            tf.tensor([0.0, 1.0, 0.0, 1.0]),
-            tf.tensor([1.0, 0.0, 1.0, 1.0]),
-            tf.tensor([1.0]),
+            [0.0, 1.0, 0.0, 1.0],
+            [1.0, 0.0, 1.0, 1.0],
+            [1.0],
         ),
         (
-            tf.tensor([0.0, 0.0, 1.0, 0.0]),
-            tf.tensor([0.0, 1.0, 1.0, 0.0]),
-            tf.tensor([1 / 3]),
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 1.0, 1.0, 0.0],
+            [1 / 3],
         ),
     ],
 )
-def test_false_positive_rate(y_true: Tensor, y_hat: Tensor, true_fpr: Tensor):
+@pytest.mark.parametrize(
+    "backend",
+    (
+        pytest.param("simple"),
+        pytest.param("fast"),
+    ),
+)
+def test_false_positive_rate(
+    y_true: List[float],
+    y_hat: List[float],
+    expected_fpr: List[float],
+    backend: str,
+):
+    y_true = tf.tensor(y_true, backend=BACKENDS[backend])
+    y_hat = tf.tensor(y_hat, backend=BACKENDS[backend])
+    expected_fpr = tf.tensor(expected_fpr, backend=BACKENDS[backend])
     predicted_fpr = false_positive_rate(y_true, y_hat)
-    assert bool(predicted_fpr.is_close(true_fpr).item())
+    assert bool(predicted_fpr.is_close(expected_fpr).item())
