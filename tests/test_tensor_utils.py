61eaa8e73ffbef8f1eebe9e4d8358665c8ff83c2
Lukas Malms <lukas.malms@gmail.com>
2022-12-06 21:50:32 +0000

    test tensor utils with different backends

--- tests/test_tensor_utils.py
+++ tests/test_tensor_utils.py
@@ -1,40 +1,64 @@
+from typing import List
+
 import pytest
 
 import minitorch.autodiff.tensor_functions as tf
-from minitorch.autodiff import Tensor
+from minitorch.autodiff import FastOps, SimpleOps, Tensor, TensorBackend
+from minitorch.autodiff.tensor_data import Shape
 from minitorch.tensor_losses import _check_dims
 from minitorch.tensor_metrics import _check_for_binary_values
 
+# Define backends
+BACKENDS = {"simple": TensorBackend(SimpleOps), "fast": TensorBackend(FastOps)}
+
 
 @pytest.mark.parametrize(
-    ["x", "y"],
-    [
-        (tf.ones((5, 1)), tf.ones((5, 1))),
-        (tf.ones((1, 1)), tf.ones((1, 1))),
-    ],
+    ["x_shape", "y_shape"],
+    [((5, 1), (5, 1)), ((1, 1), (1, 1))],
 )
-def test_equal_dims_check(x: Tensor, y: Tensor) -> None:
+@pytest.mark.parametrize(
+    "backend",
+    (pytest.param("simple"), pytest.param("fast")),
+)
+def test_equal_dims_check(x_shape: Shape, y_shape: Shape, backend: str) -> None:
+    x = tf.ones(x_shape, backend=BACKENDS[backend])
+    y = tf.ones(y_shape, backend=BACKENDS[backend])
     _check_dims(x, y)
 
 
 @pytest.mark.xfail
 @pytest.mark.parametrize(
-    ["x", "y"],
+    ["x_shape", "y_shape"],
     [
-        (tf.ones((5, 1)), tf.ones((4, 1))),
-        (tf.ones((1,)), tf.ones((1,))),
-        (tf.ones((1, 2)), tf.ones((1, 2))),
+        ((5, 1), (4, 1)),
+        ((1,), (1,)),
+        ((1, 2), (1, 2)),
     ],
 )
-def test_equal_dims_check_fail(x: Tensor, y: Tensor) -> None:
+@pytest.mark.parametrize(
+    "backend",
+    (pytest.param("simple"), pytest.param("fast")),
+)
+def test_equal_dims_check_fail(x_shape: Shape, y_shape: Shape, backend: str) -> None:
+    x = tf.ones(x_shape, backend=BACKENDS[backend])
+    y = tf.ones(y_shape, backend=BACKENDS[backend])
     _check_dims(x, y)
 
 
 @pytest.mark.parametrize(
     "y",
-    [tf.ones((5,)), tf.zeros((5,)), tf.tensor([1.0, 0.0, 1.0, 1.0, 0.0])],
+    [
+        [1.0, 1.0, 1.0, 1.0, 1.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0],
+        [1.0, 0.0, 1.0, 1.0, 0.0],
+    ],
 )
-def test_binary_values_check(y: Tensor) -> None:
+@pytest.mark.parametrize(
+    "backend",
+    (pytest.param("simple"), pytest.param("fast")),
+)
+def test_binary_values_check(y: List[float], backend: str) -> None:
+    y = tf.tensor(y, backend=BACKENDS[backend])
     _check_for_binary_values(y)
 
 
@@ -42,10 +66,15 @@ def test_binary_values_check(y: Tensor) -> None:
 @pytest.mark.parametrize(
     "y",
     [
-        tf.tensor([1.1, 0.0, 1.0, 1.0, 0.0]),
-        tf.tensor([1.0, 0.5, 1.0, 1.0, 0.0]),
-        tf.tensor([1.0, 0.3, 1.0, 1.3, 0.0]),
+        [1.1, 0.0, 1.0, 1.0, 0.0],
+        [1.0, 0.5, 1.0, 1.0, 0.0],
+        [1.0, 0.3, 1.0, 1.3, 0.0],
     ],
 )
-def test_binary_values_check_fail(y: Tensor) -> None:
+@pytest.mark.parametrize(
+    "backend",
+    (pytest.param("simple"), pytest.param("fast")),
+)
+def test_binary_values_check_fail(y: List[float], backend: str) -> None:
+    y = tf.tensor(y, backend=BACKENDS[backend])
     _check_for_binary_values(y)
